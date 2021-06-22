e3e0ad5af04e216aa8e41cd68f3249f818bc935b
Gabriel-Juan7 <gabriellbittencourtt@gmail.com>
2021-06-22 20:11:18 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let nome = prompt("Qual o seu nome? ")
+
+let idade = prompt("Qual sua idade?")
+
+let telefone = prompt("Qual seu número de telefone?")
+
+let CPF = prompt("Qual seu número de CPF?")
+
+let RG = prompt("Qual o número de seu RG?")
+
+let endereço = prompt("Qual o seu endereço?")
+
+let sexo = prompt("Qual é o seu sexo?")
+
+alert("Seu nome é: "+nome+"\nSua idade é: "+idade+"\nSeu número é: "+telefone+"\nSeu CPF é: "+CPF+"\nSeu RG é: "+RG+"\nSeu endereço é: "+endereço+"\nSeu sexo é: "+sexo)
